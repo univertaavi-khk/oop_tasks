f0c970fff968197eb4dd0ffc92ab1fa9bc6b3ab2
univertaavi-khk <univer.taavi@khk.ee>
2021-10-18 13:22:29 +0300

    Refactored LS and setting functionality

--- classes/LS.js
+++ classes/LS.js
@@ -1,4 +1,20 @@
 class LS {
+	// help functions to get and set data at LS
+	getData(name){
+		let data;
+		if(localStorage.getItem(name) === null){
+			data = [];
+		} else {
+			data = JSON.parse(localStorage.getItem(name));
+		}
+		return data;
+	}
+
+	setData(name, data){
+		localStorage.setItem(name, JSON.stringify(data))
+	}
+
+	// add task to LS
 	addTask(task){
 		// set data to the LS
 		let tasks;
